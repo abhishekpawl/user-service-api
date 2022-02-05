cc9cee8734ddef245c7c6b6bdb2bd88c9364fbc3
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-05 19:30:54 +0530

    Added static html pages to view

--- src/main/java/com/paul/userServiceApi/UserServiceApiApplication.java
+++ src/main/java/com/paul/userServiceApi/UserServiceApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @RestController
@@ -14,11 +15,10 @@ public class UserServiceApiApplication {
 	}
 
 	@GetMapping
-	public String home() {
-		return "</head><title>user-service-api</title></head><div style='margin-top: 40%;'><div style='text-align: center; margin-bottom: 2%;'><h2>user-service-api</h2></div>" +
-				"<div style='text-align: center; margin-bottom: 2%;'><a href='https://github.com/abhishekpawl/user-service-api' target='_blank'>Github Repository</a></div>" +
-				"<div style='text-align: center; margin-bottom: 2%;'><a href='./api/v1/users' target='_blank'>api/v1/users</a></div>" +
-				"<div style='text-align: center;'>API testing: <a href='https://www.postman.com/' target='_blank'>Postman</a></div></div></div>";
+	public ModelAndView index() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index.html");
+		return modelAndView;
 	}
 
 }
