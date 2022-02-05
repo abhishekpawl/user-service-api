becd29fd1035a4f683d97afc89e0232e0d1d2699
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-05 10:41:03 +0530

    Added Spring Boot project setup generated by Spring Initializr

---
+++ src/test/java/com/paul/userServiceApi/UserServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paul.userServiceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
